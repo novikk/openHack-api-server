0d28253c99a6bbfdb47125a1eb46b3478bb2fb56
Ivan de la Rubia <ivan@Ivans-MacBook-Pro.local>
2016-05-06 23:18:47 +0200

    create user model, load tmf env vars

--- controllers/default.go
+++ controllers/default.go
@@ -2,10 +2,21 @@ package controllers
 
 import (
 	"github.com/astaxie/beego"
+	"os"
 )
+
+var TMF_CUSTOMER_KEY string
+var TMF_CATALOG_KEY string
+var TMF_INVENTORY_KEY string
 
 type MainController struct {
 	beego.Controller
+}
+
+func init() {
+	TMF_CUSTOMER_KEY = os.Getenv("TMF_CUSTOMER_KEY")
+	TMF_CATALOG_KEY = os.Getenv("TMF_CATALOG_KEY")
+	TMF_INVENTORY_KEY = os.Getenv("TMF_INVENTORY_KEY")
 }
 
 func (c *MainController) Get() {
